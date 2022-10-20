d6f336b2bc66f360de51a40c58f01417b347849b
charlesK <115595915+charlesknapp@users.noreply.github.com>
2022-10-20 12:02:57 -0600

    MOOOOOOOO

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 // STRINGS 
+let firstVariable = 'Hello World';
+let secondVariable = undefined;
+let yourName = 'Charles';
 
+firstVariable = 1337;
+secondVariable = firstVariable;
+secondVariable = 'Hello Hello World';
 
+let output = `Hello, my name is ${yourName}`;
+console.log(output);
 
 //BOOLEANS
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b); //true
+console.log(c > d); //true
+console.log('Name' === 'Name'); //true
+
+//
+
+console.log(true || false); //true
+console.log(false && false && false && false && false || true); //true
+console.log(false || false) //true
+console.log(e && 'Kevin'); //true
+console.log(a + b == c); //true
+console.log(a && a || d); //true
+console.log(48 && '48'); //true
+
+let animal = prompt("Name an animal");
+if (animal != "Cow") {
+    console.log("Hey! You're not a cow!")
+    console.log("You should try Cow.... do it....")
+}
+else if (animal === "Cow") {
+    console.log("Moooooooooooooooo!")
+}
+
+
 
 //the Farm
 
